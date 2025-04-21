df63ec4fc909d7b553f7c9e3174d87a9b04fa28a
MartinZet22 <dubencak@gmail.com>
2025-04-21 20:47:00 +0200

    Přidání hodnot
    Přidaní adresy a výpočet aktuálního věku

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,29 @@ public class Main {
     public static void main(String[] args) {
 
         ProdejceMrkve prodejce1 =
-                new ProdejceMrkve("Karel Mrkvička", "12.03.1955", 12, 18.5, true);
+                new ProdejceMrkve("Karel Mrkvička", "12.03.1955", "Bezděkov 18", 12, 18.5, true);
 
         ProdejceMrkve prodejce2 =
-                new ProdejceMrkve("František Okurka", "25.11.1997", 5,6.3, false);
+                new ProdejceMrkve("František Okurka", "25.11.1997", "Lipnice 29", 5, 6.3, false);
 
         System.out.println();
 
         System.out.println("Jméno prodejce 1: " + prodejce1.getJmeno());
         System.out.println("Datum narození: " + prodejce1.getDatumNarozeni());
+        System.out.println("Věk: " + prodejce1.getVek() + " let");
+        System.out.println("Adresa: " + prodejce1.getAdresa());
         System.out.println("Počet smluv: " + prodejce1.getPocetSmluv());
-        System.out.println("Množství mrkve: " + prodejce1.getMnozstviMrkve() + "t");
+        System.out.println("Množství mrkve: " + prodejce1.getMnozstviMrkve() + " t");
         System.out.println("Je senior: " + (prodejce1.isJeSenior() ? "Ano" : "Ne"));
 
         System.out.println();
 
         System.out.println("Jméno prodejce 2: " + prodejce2.getJmeno());
         System.out.println("Datum narození: " + prodejce2.getDatumNarozeni());
+        System.out.println("Věk: " + prodejce2.getVek() + " let");
+        System.out.println("Adresa: " + prodejce2.getAdresa());
         System.out.println("Počet smluv: " + prodejce2.getPocetSmluv());
-        System.out.println("Množství mrkve: " + prodejce2.getMnozstviMrkve() + "t");
+        System.out.println("Množství mrkve: " + prodejce2.getMnozstviMrkve() + " t");
         System.out.println("Je senior: " + (prodejce2.isJeSenior() ? "Ano" : "Ne"));
 
     }
--- src/ProdejceMrkve.java
+++ src/ProdejceMrkve.java
@@ -1,19 +1,23 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class ProdejceMrkve {
     private String jmeno;
     private String datumNarozeni;
+    private String adresa;
     private int pocetSmluv;
     private double mnozstviMrkve;
     private boolean jeSenior;
 
-
-    public ProdejceMrkve(String jmeno, String datumNarozeni, int pocetSmluv, double mnozstviMrkve, boolean jeSenior) {
+    public ProdejceMrkve(String jmeno, String datumNarozeni, String adresa, int pocetSmluv, double mnozstviMrkve, boolean jeSenior) {
         this.jmeno = jmeno;
         this.datumNarozeni = datumNarozeni;
+        this.adresa = adresa;
         this.pocetSmluv = pocetSmluv;
         this.mnozstviMrkve = mnozstviMrkve;
         this.jeSenior = jeSenior;
     }
-
 
     public String getJmeno() {
         return jmeno;
@@ -29,6 +33,14 @@ public class ProdejceMrkve {
 
     public void setDatumNarozeni(String datumNarozeni) {
         this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
     }
 
     public int getPocetSmluv() {
@@ -54,5 +66,11 @@ public class ProdejceMrkve {
     public void setJeSenior(boolean jeSenior) {
         this.jeSenior = jeSenior;
     }
-}
 
+    public int getVek() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate narozeni = LocalDate.parse(datumNarozeni, formatter);
+        LocalDate dnes = LocalDate.now();
+        return Period.between(narozeni, dnes).getYears();
+    }
+}
