9682d666a75510586faafd2fd35f41ba3dd025ea
MartinZet22 <dubencak@gmail.com>
2025-04-21 20:18:53 +0200

    První verze.
    Výpis dvou prodejců (Jméno, datum narození, počet smluv, množství mrkve, jestli je prodejce senior).

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ProdejceMrkve prodejce1 =
+                new ProdejceMrkve("Karel Mrkvička", "12.03.1955", 12, 18.5, true);
+
+        ProdejceMrkve prodejce2 =
+                new ProdejceMrkve("František Okurka", "25.11.1997", 5,6.3, false);
+
+        System.out.println();
+
+        System.out.println("Jméno prodejce 1: " + prodejce1.getJmeno());
+        System.out.println("Datum narození: " + prodejce1.getDatumNarozeni());
+        System.out.println("Počet smluv: " + prodejce1.getPocetSmluv());
+        System.out.println("Množství mrkve: " + prodejce1.getMnozstviMrkve() + "t");
+        System.out.println("Je senior: " + (prodejce1.isJeSenior() ? "Ano" : "Ne"));
+
+        System.out.println();
+
+        System.out.println("Jméno prodejce 2: " + prodejce2.getJmeno());
+        System.out.println("Datum narození: " + prodejce2.getDatumNarozeni());
+        System.out.println("Počet smluv: " + prodejce2.getPocetSmluv());
+        System.out.println("Množství mrkve: " + prodejce2.getMnozstviMrkve() + "t");
+        System.out.println("Je senior: " + (prodejce2.isJeSenior() ? "Ano" : "Ne"));
+
     }
 }
--- src/ProdejceMrkve.java
+++ src/ProdejceMrkve.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
-
 public class ProdejceMrkve {
+    private String jmeno;
+    private String datumNarozeni;
+    private int pocetSmluv;
+    private double mnozstviMrkve;
+    private boolean jeSenior;
+
+
+    public ProdejceMrkve(String jmeno, String datumNarozeni, int pocetSmluv, double mnozstviMrkve, boolean jeSenior) {
+        this.jmeno = jmeno;
+        this.datumNarozeni = datumNarozeni;
+        this.pocetSmluv = pocetSmluv;
+        this.mnozstviMrkve = mnozstviMrkve;
+        this.jeSenior = jeSenior;
+    }
+
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(String datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public int getPocetSmluv() {
+        return pocetSmluv;
+    }
+
+    public void setPocetSmluv(int pocetSmluv) {
+        this.pocetSmluv = pocetSmluv;
+    }
+
+    public double getMnozstviMrkve() {
+        return mnozstviMrkve;
+    }
+
+    public void setMnozstviMrkve(double mnozstviMrkve) {
+        this.mnozstviMrkve = mnozstviMrkve;
+    }
+
+    public boolean isJeSenior() {
+        return jeSenior;
+    }
+
+    public void setJeSenior(boolean jeSenior) {
+        this.jeSenior = jeSenior;
+    }
 }
+
